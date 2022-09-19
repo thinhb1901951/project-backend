a4101f7f8f61cf6203cef9c69d0bbd09b1aee567
thinhb1901951 <thinhb1901951@student.ctu.edu.vn>
2022-09-19 10:08:12 +0700

    Project setup

--- src/app.js
+++ src/app.js
@@ -1,51 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const contactController = require('./controllers/contact.controller');
-const { clearScreenDown } = require('readline');
-const ApiError = require('./api-error');
+const express = require('express')
+const cors = require('cors')
+const contactController = require('./controllers/contact.controller')
+const ApiError = require('./api-error')
 
+const app = express()
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-
-//Handling 404 response
-app.use((req, res, next) => {
-    //handler for unknown route
-    //      call next() to pass to the error handling middleware
-    return next(new ApiError(404, 'Resources not found'))
-})
-
-//define error handling middleware last, after app.use() and routes calls
-app.use((err, req, res, next) => {
-    return res.status(err.statusCode || 500).json({
-        message: err.message || "Internal server error"
-    });
-})
-
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to contact book application!' });
-});
+    res.json({ message: 'Welcome to contact book application' })
+})
 
 app.route('/api/contacts')
     .get(contactController.findAll)
     .post(contactController.create)
-    .delete(contactController.deleteAll);
+    .delete(contactController.deleteAll)
 
-
-app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
-
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite)
 
 app.route('/api/contacts/:id')
     .get(contactController.findOne)
     .put(contactController.update)
-    .delete(contactController.delete);
+    .delete(contactController.delete)
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'))
+})
 
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+/* <<<<<<< HEAD
+        message: err.message || 'Internal Server Error'
+=======
+        message: err.message || 'Internal Server Error',
+>>>>>>> 345807d (Implement handlers) */
+    })
+})
 
-
-
-module.exports = app;
+module.exports = app
--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,28 +1,128 @@
+const ContactService = require('../services/contact.service')
+const ApiError = require('../api-error')
 
-exports.create = (req, res) => {
-    return res.send({ message: 'create handler' });
-};
+exports.create = async (req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, 'Name can not be empty'))
+    }
 
-exports.findAll = (req, res) => {
-    return res.send({ message: 'findAll handler' });
-};
+    try {
+        const contactService = new ContactService()
+        const contact = await contactService.create(req.body)
+        return res.send(contact)
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(500, 'An error occurred while creating the contact')
+        )
+    }
+}
 
-exports.findOne = (req, res) => {
-    return res.send({ message: 'findOne handler' });
-};
+exports.findAll = async (req, res, next) => {
+    let contacts = []
 
-exports.update = (req, res) => {
-    return res.send({ message: 'update handler' });
-};
+    try {
+        const contactService = new ContactService()
+        const { name } = req.query
+        if (name) {
+            contacts = await contactService.findByName(name)
+        } else {
+            contacts = await contactService.all()
+        }
 
-exports.delete = (req, res) => {
-    return res.send({ message: 'delete handler' });
-};
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(500, 'An error occurred while retrieving contacts')
+        )
+    }
 
-exports.deleteAll = (req, res) => {
-    return res.send({ message: 'deleteAll handler' });
-};
+    return res.send(contacts)
+}
 
-exports.findAllFavorite = (req, res) => {
-    return res.send({ message: 'findAllFavorite handler' });
-};
+exports.findOne = async (req, res, next) => {
+    try {
+        const contactService = new ContactService()
+        const contact = await contactService.findById(req.params.id)
+        if (!contact) {
+            return next(new ApiError(404, 'Contact not found'))
+        }
+        return res.send(contact)
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(
+                500,
+                `Error retrieving contact with id=${req.params.id}`
+            )
+        )
+    }
+}
+
+exports.update = async (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        return next(new ApiError(400, 'Data to update can not be empty'))
+    }
+
+    try {
+        const contactService = new ContactService()
+        const updated = await contactService.update(req.params.id, req.body)
+        if (!updated) {
+            return next(new ApiError(404, 'Contact not found'))
+        }
+        return res.send({ message: 'Contact was updated successfully' })
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(500, `Error updating contact with id=${req.params.id}`)
+        )
+    }
+}
+
+exports.delete = async (req, res, next) => {
+    try {
+        const contactService = new ContactService();
+        const deleted = await contactService.delete(req.params.id)
+        if (!deleted) {
+            return next(new ApiError(404, 'Contact not found'))
+        }
+        return res.send({ message: 'Contact was deleted successfully' })
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(
+                500,
+                `Could not deleted contact with id=${req.params.id}`
+            )
+        )
+    }
+}
+
+exports.deleteAll = async (req, res, next) => {
+    try {
+        const contactService = new ContactService()
+        const deleted = await contactService.deleteAll()
+        return res.send({
+            message: `${deleted} contacts were deleted successfully`
+        })
+    } catch(error) {
+        console.log(error)
+        return next(
+            new ApiError(500, 'An error occurred while removing all contacts')
+        )
+    }
+}
+
+exports.findAllFavorite = async (req, res, next) => {
+    try {
+        const contactService = new ContactService()
+        const contacts = await contactService.allFavorite()
+        return res.send(contacts)
+    } catch(error) {
+        console.log(error)
+        return next(
+            500,
+            'An error occurred while retrieving favorite contacts'
+        )
+    }
+}
